8de8a79d17672fccf5beee9fc478fcab6012da50
chibwesamuel <smchibwe@yahoo.com>
2024-09-29 17:23:29 +0200

    app - fixed rendering issues

--- smc-raindata/src/App.tsx
+++ smc-raindata/src/App.tsx
@@ -1,26 +1,14 @@
-/// <reference types="react-router-dom" />
-
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import PredictionPage from './components/PredictionPage';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <Route>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/predict" element={<PredictionPage />} />
-          </Route>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 };
 
--- smc-raindata/src/components/Dashboard.tsx
+++ smc-raindata/src/components/Dashboard.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Dashboard: React.FC = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Rainfall Dashboard</h1>
-      {/* Add dashboard content here */}
+      <h1>Dashboard is rendering!</h1>
     </div>
   );
 };

7ce763d0462b7ef395dcb384347507fcb041f84f
chibwesamuel <smchibwe@yahoo.com>
2024-09-30 08:44:36 +0200

    App - Updated App.py to render correctly

--- smc-raindata/src/App.tsx
+++ smc-raindata/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Dashboard from './components/Dashboard';
 
 const App: React.FC = () => {
